1323c36eff96e311207ccff273f02a4877899daa
Alexander Ivanov <4lex1v@gmail.com>
2014-11-17 18:36:11 +0300

    Rework modes

--- configs/modes.el
+++ configs/modes.el
@@ -1,40 +1,22 @@
 ;; Config for plugins and modes
 
-(defun 4lex1v/enable-plugins (plugins)
-	;; load provided packages 
-	(mapc (lambda (plugin) (require plugin))
-				plugins)
-	
-	;; load custom configs
-	(mapc (lambda (config-file) (load config-file))
-					(directory-files "~/.emacs.d/configs/modes/" t ".el")))
-					
+(defconst custom-config-files (directory-files "~/.emacs.d/configs/modes/" t ".el"))
+						
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(4lex1v/enable-plugins
- '(
-	 org-install 
-	 
-	 ;; Scala lang
-	 scala-mode2
-	 sbt-mode
-	 ensime
+(require 'org-install)
+(require 'projectile)
+(require 'helm-config)
+(require 'helm-projectile)
+(require 'helm-descbinds)
+(require 'yasnippet)
+(require 'scala-mode2)
+(require 'sbt-mode)
+(require 'ensime)
+(require 'haskell-mode)
+(require 'smartparens-config)
+(require 'ace-jump-mode)
+(require 'auto-complete-config)
 
-	 ;; Haskell lang
-	 haskell-mode
-
-	 ;; Helm and Projectile 
-	 projectile
-	 helm-config
-	 helm-projectile
-	 helm-descbinds
-
-	 ;; YASnippet
-	 yasnippet
-
-	 smartparens-config
-	 
-	 ;; Others
-	 ace-jump-mode
-	 auto-complete-config))
+(mapc 'load custom-config-files)
 
 (provide 'modes)
