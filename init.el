387ed55cdb3d69c48de0b1581ccee98ebbf262bf
Alexander Ivanov <4lex1v@gmail.com>
2014-11-17 12:58:34 +0300

    Align code

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 (add-to-list 'load-path (expand-file-name "configs" user-emacs-directory))
 
 (require 'packages)  ;; init package system
-(require 'general)	 ;; general emacs configs
-(require 'ui)				 ;; load custom UI configs
-(require 'modes)		 ;; configs for installed modes & plugins, but doesn't contains key bindings
+(require 'general)   ;; general emacs configs
+(require 'ui)        ;; load custom UI configs
+(require 'modes)     ;; configs for installed modes & plugins, but doesn't contains key bindings
 (require 'functions) ;; loads all custom defined functions 
-(require 'keys)			 ;; general keybindings & different mode specific
+(require 'keys)      ;; general keybindings & different mode specific
