1370ba0a458cfa39f1ee1d7e5c99552ba5085ac5
Alexander Ivanov <4lex1v@gmail.com>
2014-11-17 20:38:11 +0300

    Remove bindings for obsolete modes, add expand-region keybinding, align file

--- configs/keys.el
+++ configs/keys.el
@@ -1,7 +1,7 @@
 ;; GENERAL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key (kbd "M-j") 'join-line)
-(global-set-key (kbd "C-c m") 'execute-extended-command)
+(global-set-key (kbd "RET")     'newline-and-indent)
+(global-set-key (kbd "M-j")     'join-line)
+(global-set-key (kbd "C-c m")   'execute-extended-command)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; PROJECTILE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -14,28 +14,29 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(global-set-key (kbd "C-c h o") 'helm-occur)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-1") 'helm-projectile)
-(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-c h o")    'helm-occur)
+(global-set-key (kbd "M-y")        'helm-show-kill-ring)
+(global-set-key (kbd "C-x b")      'helm-mini)
+(global-set-key (kbd "C-x C-f")    'helm-find-files)
+(global-set-key (kbd "M-1")        'helm-projectile)
+(global-set-key (kbd "M-x")        'helm-M-x)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-(define-key helm-map (kbd "C-o") 'helm-next-source)
-(define-key helm-map (kbd "M-o") 'helm-previous-source)
-	
+(define-key helm-map (kbd "C-i")   'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")   'helm-select-action) ; list actions using C-z
+(define-key helm-map (kbd "C-o")   'helm-next-source)
+(define-key helm-map (kbd "M-o")   'helm-previous-source)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; SMEX ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;; ACE-JUMP-MODE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
 
 ;; NEOTREE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key [f8] 'neotree-toggle)
+;; (global-set-key [f8] 'neotree-toggle)
 
 ;; HIDESHOW ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-c [") 'hs-hide-block)
