f239817621c7afccfb994558fec74ea648211139
Alexander Ivanov <4lex1v@gmail.com>
2014-11-17 18:36:31 +0300

    Add benchmark for emacs startup time

--- init.el
+++ init.el
@@ -3,9 +3,10 @@
 ;; Load configuration files  
 (add-to-list 'load-path (expand-file-name "configs" user-emacs-directory))
 
-(require 'packages)  ;; init package system
-(require 'general)   ;; general emacs configs
-(require 'ui)        ;; load custom UI configs
-(require 'modes)     ;; configs for installed modes & plugins, but doesn't contains key bindings
-(require 'functions) ;; loads all custom defined functions 
-(require 'keys)      ;; general keybindings & different mode specific
+(require 'packages)       ;; init package system
+(require 'benchmark-init) ;; benchmark emacs loading time
+(require 'general)        ;; general emacs configs
+(require 'ui)             ;; load custom UI configs
+(require 'modes)          ;; configs for installed modes & plugins, but doesn't contains key bindings
+(require 'functions)      ;; loads all custom defined functions 
+(require 'keys)           ;; general keybindings & different mode specific
