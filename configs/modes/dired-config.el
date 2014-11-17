0a38509a672174efc9f76b715d2eab18c81acaa2
Alexander Ivanov <4lex1v@gmail.com>
2014-11-17 12:49:39 +0300

    New configs for fired and ibuffer

---
+++ configs/modes/dired-config.el
@@ -0,0 +1,4 @@
+;; DIRED
+
+;; Load dired-x
+(add-hook 'dired-load-hook (lambda () (load "dired-x")))
---
+++ configs/modes/ibuffer-config.el
@@ -0,0 +1,5 @@
+;; IBUFFER
+(setq ibuffer-default-sorting-mode 'major-mode)
+
+;; Hide empty groups
+(setq ibuffer-show-empty-filter-groups nil)
