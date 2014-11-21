83a26da60cf41dde44fa4ea074cd532d566890c7
Alexander Ivanov <4lex1v@gmail.com>
2014-11-22 01:59:34 +0300

    Return projectile global mode

--- configs/modes/helm-projectile-modes.el
+++ configs/modes/helm-projectile-modes.el
@@ -1,3 +1,4 @@
+(projectile-global-mode)
 (setq helm-quick-update                      t ; do not display invisible candidates
       helm-split-window-in-side-p            t ; open helm buffer inside current window, not occupy whole other window
       helm-buffers-fuzzy-matching            t ; fuzzy matching buffer names when non--nil
