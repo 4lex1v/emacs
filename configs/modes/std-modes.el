31fd2fc33087b4107b5518f19d57def2c4306b40
Alexander Ivanov <4lex1v@gmail.com>
2014-11-24 23:10:56 +0300

    small cleanup

--- configs/modes/std-modes.el
+++ configs/modes/std-modes.el
@@ -6,25 +6,5 @@
 (setq yas-snippet-dirs '("~/.emacs.d/snippets")) ;; personal snippets
 (yas-reload-all) ;; we need to reload yas after adding custom dir
 
-;; DIRED
-;; (setq delete-by-moving-to-trash t)
-
-;; IDO
-;; (ido-mode 0)
-;; (ido-everywhere 0)
-;; (setq ido-use-faces nil)
-;; (setq ido-enable-flex-matching t)
-
-;; SMEX
-;; Extends std M-x functionality providing some history, better command
-;; search capabilities + can show commands only for the current major mode
-;; Still using for mode-private commands M-X
-;; (smex-initialize)
-
-;; AUTO-COMPLETE
-;; (add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20140726.303")
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20140726.303/ac-dict")
-(ac-config-default)
-
 ;; SMARTPARENS
 (setq sp-highlight-pair-overlay nil)
