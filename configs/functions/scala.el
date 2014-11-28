1118a298b4d9f02dfa1a2941c7fa8e3dd29643aa
Alexander Ivanov <4lex1v@gmail.com>
2014-11-28 20:23:28 +0300

    rename function

--- configs/functions/scala.el
+++ configs/functions/scala.el
@@ -1,5 +1,5 @@
-;; IN multiline comments inserts a new line with indented asterisk 
-(defun scala-functions:new-comment-line ()
+;; In multiline comments inserts a new line with indented asterisk 
+(defun scala-functions:newline-or-comment ()
   (interactive)
   (indent-new-comment-line)
   (scala-indent:insert-asterisk-on-multiline-comment))
