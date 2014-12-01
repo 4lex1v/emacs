ca4acbb8dc1c479730243953123ed78780d00860
Alexander Ivanov <4lex1v@gmail.com>
2014-12-01 15:04:11 +0300

    Load ensime only for ensime projects

--- configs/modes/scala-config.el
+++ configs/modes/scala-config.el
@@ -1,13 +1,12 @@
 ;; Scala-mode configuration
 
-(add-hook 'scala-mode-hook 
-  '(lambda ()
-		 (yas-minor-mode)
-		 (hs-minor-mode)
-		 (ensime-scala-mode-hook)
-		 (smartparens-mode)))
-
-;; Scala-mode smartparens configuration
+;; Load ensime mode for scala only if there is an ensime
+;; project file .ensime defined in the root directory
+(defun 4lex1v/smart-ensime-loading ()
+  (let* ((root-dir            (projectile-project-root))
+         (ensime-project-file (concat root-dir ".ensime"))
+         (ensime-project?     (file-exists-p ensime-project-file)))
+    (if ensime-project? (ensime-scala-mode-hook))))
 
 ;; Indent new line between braces with smartparens mode
 (defun 4lex1v/indent-in-braces (&rest _ignored)
@@ -17,7 +16,15 @@
   (forward-line -1)
   (indent-according-to-mode))
 
-;; Smartparen configuration for scala-mode
-(sp-local-pair 'scala-mode "{" nil :post-handlers '((4lex1v/indent-in-braces "RET")))
+(add-hook 'scala-mode-hook 
+          '(lambda ()
+             (yas-minor-mode)
+             (hs-minor-mode)
+             (4lex1v/smart-ensime-loading)
+             (smartparens-mode)))
 
+
+;; Smartparen configuration for scala-mode
+(sp-local-pair 'scala-mode "{" nil
+               :post-handlers '((4lex1v/indent-in-braces "RET")))
 (setq scala-indent:use-javadoc-style t)
