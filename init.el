9f5d0ad354051649b021b0c87e036e8aaafc7d93
Alexander Ivanov <4lex1v@gmail.com>
2015-04-10 16:07:21 +0300

    New shape of emacs init file

--- init.el
+++ init.el
@@ -1,12 +1,25 @@
 ;;;;;;;;; EMACS SETTINGS ;;;;;;;;;;
 
-;; Load configuration files  
-(add-to-list 'load-path (expand-file-name "configs" user-emacs-directory))
+;; Custom configurations folder
+(defconst configs-dir   (expand-file-name "configs" user-emacs-directory))
+(defconst themes-dir    (expand-file-name "themes"  user-emacs-directory))
 
-(require 'packages)       ;; init package system
-(require 'benchmark-init) ;; benchmark emacs loading time
-(require 'general)        ;; general emacs configs
-(require 'ui)             ;; load custom UI configs
-(require 'modes)          ;; configs for installed modes & plugins, but doesn't contains key bindings
-(require 'functions)      ;; loads all custom defined functions 
-(require 'keys)           ;; general keybindings & different mode specific
+;; General settings
+(defconst custom-themes (directory-files themes-dir nil "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))
+(defconst config-files  (directory-files "~/.emacs.d/configs/modes/" t ".el"))
+
+;; Load configs
+(add-to-list 'load-path configs-dir)
+(add-to-list 'load-path "~/.emacs.d/modes.el")
+
+(require 'packages)         ;; init package system
+(require 'benchmark-init)   ;; benchmark emacs loading time
+(benchmark-init/activate)   ;; start benchmarking
+
+(require 'use-package)
+
+(use-package ui)            ;; Load UI settings
+(use-package general)       ;; general emacs configs
+(use-package modes)         ;; configs for installed modes & plugins, but doesn't contains key bindings
+
+(benchmark-init/deactivate) ;; stop benchmarking
