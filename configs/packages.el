2b59bc241df028e652e888d2570d9b1dad269b43
Alexander Ivanov <4lex1v@gmail.com>
2015-05-13 15:49:01 +0300

    Cleanup

--- configs/packages.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-
-;; Configure repositories
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(package-initialize)
-
-(provide 'packages)
