32e053d6fad35b7a4ebe6dfa8a7d00c1f29b3c9b
Aleksandr <4lex1v@gmail.com>
2016-02-01 16:29:57 -0500

    Web-mode prefixes

--- init.el
+++ init.el
@@ -492,16 +492,26 @@
 
 (use-package web-mode
   :load-path "packages/web"
+
   :mode (("\\.html\\'" . web-mode)
          ("\\.html\\.erb\\'" . web-mode)
          ("\\.mustache\\'" . web-mode)
          ("\\.jinja\\'" . web-mode)
          ("\\.php\\'" . web-mode))
-  :init (setq web-mode-engines-alist '(("\\.jinja\\'"  . "django"))
-              web-mode-enable-auto-pairing t
-              web-mode-enable-css-colorization t
-              web-mode-enable-current-element-highlight t
-              web-mode-enable-current-column-highlight nil))
+
+  :init (progn
+          (setq web-mode-engines-alist '(("\\.jinja\\'"  . "django"))
+                web-mode-enable-auto-pairing t
+                web-mode-enable-css-colorization t
+                web-mode-enable-current-element-highlight t
+                web-mode-enable-current-column-highlight nil)
+
+          (which-key-declare-prefixes
+            "C-c C-t" "web/tag"
+            "C-c C-b" "web/block"
+            "C-c C-d" "web/dom"
+            "C-c C-a" "web/attribute"
+            "C-c C-e" "web/element")))
 
 (use-package docker
   :load-path "core/docker"
