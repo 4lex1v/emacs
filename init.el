09cffdda3fc4fafa4e661be6fba8d04db8a9d5a9
Aleksandr Ivanov <4lex1v@gmail.com>
2016-12-12 09:33:51 +0300

    Remove dired+

--- init.el
+++ init.el
@@ -140,10 +140,6 @@
   :defer 2
   :load-path "core/f")
 
-(use-package dired+
-  :init (setq dired-use-ls-dired nil
-              diredp-toggle-find-file-reuse-dir nil))
-
 (use-package ranger
   :load-path "core/ranger"
   :bind ("C-x C-r" . ranger)
