d15add77bd6a7221ce987575f8cf13fd6ffac2d6
Aleksandr Ivanov <4lex1v@gmail.com>
2016-12-28 21:09:20 +0300

    Remove elisp

--- packages/elisp/elisp.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'load-path (concat user-emacs-directory "packages/elisp/macrostep"))
-
-(defun wrap-with-parens (&optional arg)
-  (interactive "P")
-  (sp-wrap-with-pair "(")
-  (sp-forward-slurp-sexp))
-
-(provide 'elisp)
