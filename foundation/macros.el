856fd1b9ac4a3b2e1dc29e97121014916d6f9113
Aleksandr Ivanov <4lex1v@gmail.com>
2017-01-09 10:47:47 +0300

    Fix macros

--- foundation/macros.el
+++ foundation/macros.el
@@ -1,9 +1,8 @@
 (defmacro with-package (pkg-name &rest body)
   (declare (indent 1))
-  `(unless (fboundp ',pkg-name) (progn ,@body)))
+  `(if (fboundp ',pkg-name) (progn ,@body)))
 
 (defmacro with-mode (mode-name &rest body)
   (declare (indent 1))
-  (let ((mode-symb (make-symbol (format "%s-mode" mode-name))))
-    `(unless (fboundp ',mode-symb) (progn ,@body))))
-
+  (let ((mode-symb (intern (format "%s-mode" mode-name))))
+    `(if (fboundp ',mode-symb) (progn ,@body))))
