cba185c1139b75a54eeffa2be07bb9fe848979af
Aleksandr <4lex1v@gmail.com>
2017-02-23 13:34:10 +0300

    Add Slack package to the system

--- modules/communications/install.el
+++ modules/communications/install.el
@@ -1,6 +1,14 @@
 (use-package jabber
-  :load-path "packages/emacs-jabber"
+  :load-path "modules/communications/jabber"
+  :after exec-path-from-shell
   :init
   (setq jabber-account-list
         `(("4lex1v@livecoding.tv"
            (:password . ,(exec-path-from-shell-getenv "LC_PWD"))))))
+
+;; TODO :: should this be here?
+(use-package oauth2 :ensure t)
+
+(use-package slack
+  :after oauth2
+  :load-path "modules/communications/slack")
