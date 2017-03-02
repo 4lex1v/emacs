10e8f56943b7551be39c4cfebf089b60f9d1c235
Aleksandr <4lex1v@gmail.com>
2017-03-02 11:17:14 +0000

    Small cleanup

--- init.el
+++ init.el
@@ -1,19 +1,18 @@
 (setq gc-cons-threshold 10000000
       package--init-file-ensured t
-      debug-on-error t)
+      debug-on-error nil)
 
 ;; At this point i'm not sure what's the better way to solve this chiken-and-egg issue
 ;; There's a corresponding comment in org module.org config file
 (add-to-list 'load-path (expand-file-name "modules/org/org-mode/lisp" user-emacs-directory))
 (load (expand-file-name "foundation/foundation" user-emacs-directory))
 
-;; Emacs experience configs 
 (fnd:platform-module essentials)
 
 ;; So `vim' module goes before others to give access to `evil-leader' configuration
 ;; Conceptually, it's something i'd move to `essentials' as a config (e.g `:vim t')
 ;; Leaving it this way for now
-(fnd:platform-module vim) ;; Enabled initially, turn-off manually if needed
+(fnd:platform-module vim)
 
 (fnd:platform-module appearance)
 (fnd:platform-module behaviour)
