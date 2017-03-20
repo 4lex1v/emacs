3cbd47f727f973ca674ac12371672008fcbb526d
Aleksandr <4lex1v@gmail.com>
2017-03-20 19:15:52 +0300

    [Root] Quick cleanup

--- init.el
+++ init.el
@@ -38,19 +38,5 @@
 (fnd:install-module formats)
 (fnd:install-module statistics)
 (fnd:install-module communications)
-;(fnd:install-module reader)
 
 (setq gc-cons-threshold 100000)
-
-;; change mode-line color by evil state
-;; (lexical-let ((default-color (cons (face-background 'mode-line)
-;;                                    (face-foreground 'mode-line))))
-;;   (add-hook 'post-command-hook
-;;             (lambda ()
-;;               (let ((color (cond ((minibufferp) default-color)
-;;                                  ((evil-insert-state-p) '("#e80000" . "#ffffff"))
-;;                                  ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-;;                                  ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
-;;                                  (t default-color))))
-;;                 (set-face-background 'mode-line (car color))
-;;                 (set-face-foreground 'mode-line (cdr color))))))
