6b0fd9195a8bae3efa96f4e08acaf3662673fc21
Aleksandr <4lex1v@gmail.com>
2017-03-22 10:16:24 +0300

    [Init] Improve Init.el

--- init.el
+++ init.el
@@ -1,40 +1,44 @@
 (setq gc-cons-threshold 10000000)
 
-;; At this point i'm not sure what's the better way to solve this chiken-and-egg issue
-;; There's a corresponding comment in org module.org config file
-(add-to-list 'load-path (expand-file-name "modules/org/org-mode/lisp" user-emacs-directory))
-(load (expand-file-name "foundation/foundation" user-emacs-directory))
+(eval-and-compile
+  (load (expand-file-name "foundation/foundation" user-emacs-directory)))
 
-(fnd:platform-module essentials)
+;;
+;;; Platform Configuration
+;; 
 
-;; So `vim' module goes before others to give access to `evil-leader' configuration
-;; Conceptually, it's something i'd move to `essentials' as a config (e.g `:vim t')
-;; Leaving it this way for now
-(fnd:platform-module vim)
+(fnd:module essentials     "The basis for my `.emacs.d' configuration. Configures package-management system and `use-package', along with others less important settings.")
 
-(fnd:platform-module appearance)
-(fnd:platform-module behaviour)
-(fnd:platform-module editor)
-(fnd:platform-module system)
+(fnd:module appearance     "Themes, fonts, mode-line and other fancy things for a better look")
+(fnd:module vim            "Platform configuration for EVIL layer")
+(fnd:module behaviour      "All-things-navigation & movement")
+(fnd:module editor         "A lot of things to improve editing experience")
+(fnd:module system         "Low-level configuration for a better OS <=> Emacs integration")
 
-;; Modules: Languages
-(fnd:extension elisp)
-(fnd:extension scala)
-(fnd:extension haskell)
-(fnd:extension idris)
-(fnd:extension purescript)
-(fnd:extension native)
-(fnd:extension clojure)
-(fnd:extension groovy)
-(fnd:extension python)
-(fnd:extension web)
+;;
+;;; Modules: Languages
+;;
 
-;; Modules: Others
-(fnd:extension org)
-(fnd:extension vcs)
-(fnd:extension infra)
-(fnd:extension formats)
-(fnd:extension statistics)
-(fnd:extension communications)
+(fnd:module elisp          "Configuration for `emacs-lisp-mode' and other helpfull stuff to improve the experience with `ELisp'")
+(fnd:module scala          "Scala language support + Ensime language server")
+(fnd:module haskell        "Haskell programming language support")
+(fnd:module idris          "Idris programming language support")
+(fnd:module purescript     "PureScript programming language support")
+(fnd:module native         "C/C++ support")
+(fnd:module clojure        "Clojure programming language support")
+(fnd:module groovy         "Groovy programming language support. Require for various DevOps related stuff, e.g Jenkinsfiles")
+(fnd:module python         "Extensive support for Python ecosystem")
+
+;;
+;;; Modules: Others
+;;
+
+(fnd:module org            "Org-mode configuration package")
+(fnd:module web            "Web & Frontend engineering stuff")
+(fnd:module vcs            "All-things-Magit")
+(fnd:module infra          "DevOps related stuff, e.g Docker")
+(fnd:module formats        "Minor formats like Markdown & YAML support")
+(fnd:module statistics     "Configuration package for math & statistics related tasks")
+(fnd:module communications "Slack, Jabber, etc...")
 
 (setq gc-cons-threshold 100000)
