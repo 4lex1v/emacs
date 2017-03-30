9844540e199ca4b694e0a796802b034341915af1
Aleksandr <4lex1v@gmail.com>
2017-03-30 15:10:35 +0300

    [Vcs] Small config improvements

--- modules/vcs/install.el
+++ modules/vcs/install.el
@@ -1,7 +1,6 @@
 (use-package magit
   :load-path "modules/vcs/magit/lisp"
   :after eshell
-  :commands (magit-status magit-clone magit-submodule-popup)
   :init
   (setq-default
    magit-submodule-list-columns
@@ -23,42 +22,43 @@
         magit-diff-options '("--word-diff")
         magit-diff-refine-hunk 'all) ;; experimental
 
+  (evil-set-initial-state 'magit-submodule-list-mode 'emacs)
+  
+  :general 
+  ("m" '(:ignore t :which-key "Magit")
+   "ms"  'magit-status
+   "mm"  'magit-dispatch-popup
+   "mb"  'magit-blame
+   "mo"  'magit-submodule-popup
+   "my"  'magit-show-refs-popup
+   "me"  'magit-ediff-popup
+   "mp"  'magit-push-popup
+   "mf"  'magit-pull-popup
+   "ml" '(:ignore t :which-key "Logging")
+   "mll" 'magit-log-all
+   "mlb" 'magit-log-buffer-file
+   "mlc" 'magit-log-current)
+
   :config
   (add-to-list 'magit-log-arguments "--color")
 
   (magit-define-popup-action 'magit-submodule-popup   
-    ?l "List" 'magit-list-submodules)
-
-  (evil-initial-state 'magit-submodule-list-mode 'emacs))
+    ?l "List" 'magit-list-submodules))
 
 (use-package with-editor
   :after magit
   :ensure t
-  :defer)
+  :defer
+  :general
+  (:keymaps 'with-editor-mode-map
+    :prefix "" ;; don't use SPC prefix in this case
+    "RET"    'with-editor-finish
+    [escape] 'with-editor-cancel)
+  :init
+  (evil-set-initial-state 'with-editor-mode 'insert))
 
 (use-package evil-magit
   :after magit-mode
-  :init 
-  (general-define-key
-    "m" '(:ignore t :which-key "Magit")
-    "ms"  'magit-status
-    "mm"  'magit-dispatch-popup
-    "mb"  'magit-blame
-    "mo"  'magit-submodule-popup
-    "my"  'magit-show-refs-popup
-    "me"  'magit-ediff-popup
-    "mp"  'magit-push-popup
-    "mf"  'magit-pull-popup
-    "ml" '(:ignore t :which-key "Logging")
-    "mll" 'magit-log-all
-    "mlb" 'magit-log-buffer-file
-    "mlc" 'magit-log-current)
-
-  (general-define-key :keymaps 'with-editor-mode-map
-    "RET" 'with-editor-finish
-    [escape]    'with-editor-cancel)
-
-  (add-hook 'with-editor-mode-hook 'evil-insert-state)
   :config
   (evil-magit-init))
 
