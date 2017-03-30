09497a86d448cc1b6498a0a8157e7b53898b956b
Aleksandr <4lex1v@gmail.com>
2017-03-30 14:28:06 +0300

    [Scala] Update package and configuration

--- modules/scala/install.el
+++ modules/scala/install.el
@@ -1,47 +1,47 @@
-
-(load "defs")
-
 (use-package scala-mode
-  :load-path "modules/scala/scala-mode"
-  :after (yasnippet company)
-  :commands scala-mode
-  :mode ("\\.\\(scala\\|sbt\\|sc\\)\\'" . scala-mode)
+  :defer
+  :mode        ("\\.\\(scala\\|sbt\\|sc\\)\\'" . scala-mode)
   :interpreter ("scala" . scala-mode)
 
   :bind
   (:map scala-mode-map
-   ("C-c b"         . sbt-ext:open-build-file)
    ("<C-return>"    . newline-or-comment)
    ("M-j"           . scala-indent:join-line)
-   ("C-c c"         . sbt-command)
    ("C-<backspace>" . contextual-backspace))
+  
+  :general
+  (:keymaps 'scala-mode-map
+    "s" '(:ignore t :which-key "Scala"))
 
   :init
-  (setq-default initial-major-mode 'scala-mode)
   (setq scala-indent:use-javadoc-style t
         scala-mode:debug-messages nil)
 
-  :config
-  (with-package smartparens-mode
-    (use-package smartparens-scala
-      :init
-      (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
-      (sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))))
-  
-  (with-package hideshowvis
-    (push '(scala-mode "\\({\\|(\\)" "\\(}\\|)\\)" "/[*/]" nil nil)
-          hs-special-modes-alist))
+  (add-hook 'scala-mode-hook #'yas-minor-mode)
+  (add-hook 'scala-mode-hook #'global-company-mode)
+  (add-hook 'scala-mode-hook #'4lex1v/fix-scala-fonts)
 
-  (4lex1v/hook-into-modes #'4lex1v/fix-scala-fonts 'scala-mode-hook))
+  :config
+  (load "defs")
+  (push '(scala-mode "\\({\\|(\\)" "\\(}\\|)\\)" "/[*/]" nil nil) hs-special-modes-alist))
+
+(use-package smartparens-scala
+  :after scala-mode
+  :init
+  (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
+  (sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))))
 
 (use-package sbt-mode
-  :load-path "modules/scala/sbt-mode"
   :after scala-mode
-  :commands (sbt-start sbt-command))
+  :general
+  (:keymaps 'scala-mode-map
+    "sb" '(:ignore t :which-key "SBT")
+    "sbb" #'sbt-ext:open-build-file
+    "sbs" #'sbt-start
+    "sbr" #'sbt-command))
 
 (use-package ensime
-  :after scala
-  :load-path "modules/scala/ensime"
+  :after scala-mode
   :commands ensime
   :bind
   (:map scala-mode-map
@@ -56,18 +56,15 @@
         ensime-startup-notification   nil
         ensime-startup-snapshot-notification nil)
 
-  (with-mode which-key
-    (which-key-declare-prefixes-for-mode 'scala-mode
-      "C-c C-d" "ensime/debug"
-      "C-c C-c" "ensime/compiler"
-      "C-c C-r" "ensime/refactoring"
-      "C-c C-t" "ensime/tests"
-      "C-c C-v" "ensime/general"
-      "C-c C-b" "ensime/sbt"))
-  
-  (use-package popup :ensure t)
+  (setq-local eldoc-documentation-function
+              #'(lambda ()
+                  (when (ensime-connected-p)
+                    (let ((err (ensime-print-errors-at-point)))
+                      (or (and err (not (string= err "")) err)
+                          (ensime-print-type-at-point))))))
 
   (defun 4lex1v:smart-ensime-loader ()
+    (interactive)
     (lambda () 
       (if (and (if-bound-f projectile-project-p)
                (if-bound-f projectile-project-root))
@@ -76,19 +73,37 @@
           (message (format "Projectile is not loaded, using %s" default-directory))
           (4lex1v/connect-running-ensime default-directory)))))
 
+  (defun 4lex1v:update-ensime-build ()
+    (interactive)
+    (sbt-command "ensimeConfig"))
+  
+  (general-define-key :keymaps 'scala-mode-map
+    "se" '(:ignore t :which-key "Ensime")
+    "ses"  #'ensime
+    "seu"  #'4lex1v:update-ensime-build)
+  
+  (general-define-key :keymaps 'ensime-mode-map
+    "ser"  #'ensime-inf-run-scala
+    "seb" '(:ignore t :which-key "Build")
+    "sebr" #'ensime-sbt-do-run
+    "sebc" #'ensime-sbt-do-compile)
+
+  (which-key-declare-prefixes-for-mode 'ensime-mode
+    "C-c C-d" "ensime/debug"
+    "C-c C-c" "ensime/compiler"
+    "C-c C-r" "ensime/refactoring"
+    "C-c C-t" "ensime/tests"
+    "C-c C-v" "ensime/general")
+
   ;; Smart Ensime loader
   (4lex1v/hook-into-modes #'4lex1v:smart-ensime-loader 'scala-mode-hook)
 
-  ;; Eldoc support in Scala + Ensime mode
-  (setq-local eldoc-documentation-function
-              #'(lambda ()
-                  (when (ensime-connected-p)
-                    (let ((err (ensime-print-errors-at-point)))
-                      (or (and err (not (string= err "")) err)
-                          (ensime-print-type-at-point))))))
-
   :config
-  (unbind-key "M-p" ensime-mode-map)
-  (use-package ensime-company
-    :init
-    (add-to-list 'company-backends 'ensime-company)))
+  (unbind-key "M-p" ensime-mode-map))
+
+(use-package ensime-company
+  :after (ensime company))
+
+(use-package popup
+  :after scala-mode
+  :ensure t)
