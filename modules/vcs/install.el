1bdcc3ac162a8d1c5d40acb4ea49fabad59ceb06
Aleksandr <4lex1v@gmail.com>
2017-03-30 13:40:35 +0300

    [VCS] Updated

--- modules/vcs/install.el
+++ modules/vcs/install.el
@@ -1,15 +1,8 @@
 (use-package magit
   :load-path "modules/vcs/magit/lisp"
+  :after eshell
   :commands (magit-status magit-clone magit-submodule-popup)
   :init
-
-  (setq magit-last-seen-setup-instructions "2.3.2"
-        magit-status-show-hashes-in-headers t
-        magit-diff-options '("--word-diff")
-        magit-diff-refine-hunk 'all) ;; experimental
-
-  (use-package with-editor :ensure t)
-
   (setq-default
    magit-submodule-list-columns
    (quote
@@ -25,17 +18,29 @@
      ("L>P" 3 magit-repolist-column-unpushed-to-pushremote
       ((:right-align t))))))
 
+  (setq magit-last-seen-setup-instructions "2.3.2"
+        magit-status-show-hashes-in-headers t
+        magit-diff-options '("--word-diff")
+        magit-diff-refine-hunk 'all) ;; experimental
+
   :config
   (add-to-list 'magit-log-arguments "--color")
+
   (magit-define-popup-action 'magit-submodule-popup   
     ?l "List" 'magit-list-submodules)
 
   (evil-initial-state 'magit-submodule-list-mode 'emacs))
 
+(use-package with-editor
+  :after magit
+  :ensure t
+  :defer)
+
 (use-package evil-magit
   :after magit-mode
   :init 
-  (evil-leader/set-key
+  (general-define-key
+    "m" '(:ignore t :which-key "Magit")
     "ms"  'magit-status
     "mm"  'magit-dispatch-popup
     "mb"  'magit-blame
@@ -44,39 +49,31 @@
     "me"  'magit-ediff-popup
     "mp"  'magit-push-popup
     "mf"  'magit-pull-popup
+    "ml" '(:ignore t :which-key "Logging")
     "mll" 'magit-log-all
     "mlb" 'magit-log-buffer-file
     "mlc" 'magit-log-current)
 
-  (which-key-declare-prefixes
-    "<SPC> m"   "Magit"
-    "<SPC> m l" "Logging") 
-
-  (evil-define-key 'normal with-editor-mode-map
-    (kbd "RET") 'with-editor-finish
+  (general-define-key :keymaps 'with-editor-mode-map
+    "RET" 'with-editor-finish
     [escape]    'with-editor-cancel)
-
-  (evil-define-key 'normal git-rebase-mode-map
-    "l" 'git-rebase-show-commit)
 
   (add-hook 'with-editor-mode-hook 'evil-insert-state)
   :config
   (evil-magit-init))
 
 (use-package diff-hl
-  :load-path "modules/vcs/diff-hl"
+  :after magit
+  :init
+  (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh t)
   :config
-  (global-diff-hl-mode 1)
+  (global-diff-hl-mode 1))
 
-  (with-mode magit
-    (add-hook 'magit-post-refresh-hook
-              #'diff-hl-magit-post-refresh t))
-
-  (use-package diff-hl-flydiff
-    :config (diff-hl-flydiff-mode)))
+(use-package diff-hl-flydiff
+  :after diff-hl
+  :config (diff-hl-flydiff-mode))
 
 (use-package magithub
-  :load-path "modules/vcs/magithub"
   :after magit
   :config
   (magithub-feature-autoinject t))
