82dd1b9c92b061a62f861f67c3f54209b23fc87b
Aleksandr Ivanov <aleksandrivanov@Aleksandrs-MacBook-Pro.local>
2017-04-22 12:12:28 -0400

    [Essentials] Update packages config for 25.2

--- modules/essentials/install.el
+++ modules/essentials/install.el
@@ -9,17 +9,16 @@
 (require 'use-package)
 
 (require 'package)
+
 (setq package-enable-at-startup nil
-      package--init-file-ensured t)
-(add-to-list 'package-archives '(("melpa"     . "https://melpa.org/packages/")
-                                 ("marmalade" . "http://marmalade-repo.org/packages/")
-                                 ("gnu"       . "http://elpa.gnu.org/packages/")))
+      package--init-file-ensured t
+      package-archives '(("melpa"     . "https://melpa.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("gnu"       . "http://elpa.gnu.org/packages/")))
+
 (package-initialize)
 
 ;; Vendor packages
-(use-package dash     :ensure t)
-(use-package s        :ensure t)
-(use-package f        :ensure t)
 (use-package diminish :ensure t)
 
 (unbind-key "C-x b")
