5600d7272e74e74af8b612964149e7712298582c
Aleksandr Ivanov <4lex1v@gmail.com>
2017-04-26 09:53:49 -0400

    [Haskell] Improved configuration

--- modules/haskell/install.el
+++ modules/haskell/install.el
@@ -1,3 +1,12 @@
 (use-package haskell-mode
-  :defer
-  :load-path "packages/haskell/haskell-mode")
+  :mode "\\.hs\\'"
+  :defer t
+  :commands haskell-mode
+  
+  :init
+  (setq haskell-process-log t
+        haskell-process-type (quote stack-ghci))
+  
+  :config
+  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode))
