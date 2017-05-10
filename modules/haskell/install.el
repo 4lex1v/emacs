540436e10899ae5e746172e1f41c03f4110fa881
Aleksandr Ivanov <4lex1v@gmail.com>
2017-05-10 11:44:54 -0400

    [Haskell] Better configuration for Haskell mode

--- modules/haskell/install.el
+++ modules/haskell/install.el
@@ -1,12 +1,23 @@
+(use-package haskell-mode-autoloads)
+
 (use-package haskell-mode
-  :mode "\\.hs\\'"
-  :defer t
-  :commands haskell-mode
-  
+  :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
+         ("\\.lhs\\'" . literate-haskell-mode)
+         ("\\.cabal\\'" . haskell-cabal-mode))
+
   :init
   (setq haskell-process-log t
-        haskell-process-type (quote stack-ghci))
-  
+        haskell-process-type 'stack-ghci))
+
+(use-package haskell
   :config
-  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
+
+(use-package haskell-doc-mode
+  :config
+  (add-hook 'haskell-mode-hook 'haskell-doc-mode))
+
+(use-package haskell-decl-scan
+  :config
   (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode))
+
