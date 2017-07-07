84dbf557eff2ff3b578c2cb8d45ecfe4f265c8a7
Aleksandr Ivanov <4lex1v@gmail.com>
2017-07-07 10:48:25 -0400

    [Multiple] Update packages, better lazy loading

--- modules/formats/install.el
+++ modules/formats/install.el
@@ -1,15 +1,14 @@
 (use-package yaml-mode
-  :load-path "modules/formats/yaml"
   :mode ("\\.yml\\|\\.yaml$" . yaml-mode))
 
 (use-package markdown-mode
-  :load-path "modules/formats/markdown-mode"
   :commands (markdown-mode gfm-mode)
-  :mode
-  (("README\\.md\\'" . gfm-mode)
-   ("\\.md\\'"       . markdown-mode)
-   ("\\.markdown\\'" . markdown-mode))
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'"       . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
+(use-package plantuml-mode :defer)
 
 ;; (use-package pandoc-mode
 ;;   :load-path "modules/formats/pandoc-mode"
--- modules/haskell/install.el
+++ modules/haskell/install.el
@@ -1,6 +1,8 @@
-(use-package haskell-mode-autoloads)
+(use-package haskell-mode-autoloads
+  :defer)
 
 (use-package haskell-mode
+  :defer 
   :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
          ("\\.lhs\\'" . literate-haskell-mode)
          ("\\.cabal\\'" . haskell-cabal-mode))
@@ -12,14 +14,17 @@
   (evil-set-initial-state 'haskell-error-mode 'emacs))
 
 (use-package haskell
+  :defer
   :config
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
 
 (use-package haskell-doc-mode
+  :defer
   :config
   (add-hook 'haskell-mode-hook 'haskell-doc-mode))
 
 (use-package haskell-decl-scan
+  :defer
   :config
   (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode))
 
