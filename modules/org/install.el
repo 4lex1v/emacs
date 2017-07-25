55a7bfc526fad546cc5cd73b59c050199dc2a6bc
Aleksandr Ivanov <4lex1v@gmail.com>
2017-07-25 10:16:44 -0400

    [Org] Configuration update

--- modules/org/install.el
+++ modules/org/install.el
@@ -1,56 +1,46 @@
 (use-package org
   :load-path "modules/org/org-mode/lisp"
   :after flyspell
-  :bind
-  (("C-c o l" . org-store-link)
-   ("C-c o a" . org-agenda)
-   ("C-c o c" . org-capture))
-
   :bind* ("C-'"  . ace-window)
-
+  :general
+  ;; Global Org-mode fucntionality
+  ("eo" '(:ignore t :which-key "Global Org")
+   "eoc" 'org-capture
+   "eol" 'org-store-link
+   "eoa" 'org-agenda) 
+  
   :init
-  (setq org-log-done             t
-        org-src-fontify-natively t
-        org-descriptive-links    t
+  (setq org-log-done                   t
+        org-src-fontify-natively       t
+        org-descriptive-links          t
         org-startup-with-inline-images t
-        org-babel-load-languages '((emacs-lisp . t)
-                                   (scala      . t)
-                                   (haskell    . t)
-                                   (shell      . t)
-                                   (python     . t)
-                                   (clojure    . t)
-                                   (js         . t)
-                                   (racket     . t))
-        org-ellipsis "⬎"
-        org-hide-leading-stars nil
-        org-startup-indented nil
-        org-line-spacing 5
-        org-notes-font "Menlo")
+        org-ellipsis                   "⬎"
+        org-hide-leading-stars         nil
+        org-startup-indented           nil
+        org-line-spacing               5
+        org-notes-font                "Menlo"
+        
+        ;; Agenda files to cycle
+        org-agenda-files '("~/Sandbox/GTD/main.org")
+
+        ;; Templates configuration
+        org-capture-templates '(("t" "Task"    entry (file+headline "~/Sandbox/GTD/main.org" "Tasks")    "* %i%?")
+                                ("p" "Project" entry (file+headline "~/Sandbox/GTD/main.org" "Projects") "* %i%?"))
+
+        ;; Keywords
+        org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
   :config
-  (with-mode which-key
-    (which-key-declare-prefixes
-      "C-c o" "org"))
-
+  ;; Since this config depends on the runtime value, this should be configured in this section
   (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
   
-  (add-to-list 'org-structure-template-alist
-               '("scala" "#+BEGIN_SRC scala \n\t?\n#+END_SRC"))
+  (add-to-list 'org-structure-template-alist '("scala" "#+BEGIN_SRC scala \n\t?\n#+END_SRC"))
   
-  (defun enlarge-line-spacing ()
-    (setq-local line-spacing org-line-spacing))
-
-  (add-hook 'org-mode-hook #'enlarge-line-spacing)
-  
+  (add-hook 'org-mode-hook #'(lambda () (setq-local line-spacing org-line-spacing)))
   (add-hook 'org-mode-hook #'flyspell-mode)
   (add-hook 'org-mode-hook #'toggle-truncate-lines))
 
 (use-package ob :after org)
-
-;; (use-package org-bullets
-;;   :after org
-;;   :init
-;;   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (use-package org-beautify
   :after org
@@ -59,5 +49,3 @@
   (load-theme 'org-beautify t))
 
 (use-package org-attach-screenshot :after org)
-
-(setq notes-folder "~/Sandbox/Notes")
