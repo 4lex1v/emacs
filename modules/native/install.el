d28549b7f6cb27f9f18cf2bec738a1a87f990c43
Aleksandr Ivanov <4lex1v@gmail.com>
2017-07-25 10:10:00 -0400

    [Native] Improve Native langs cupport configuration

--- modules/native/install.el
+++ modules/native/install.el
@@ -1,3 +1,52 @@
-(use-package cmake-mode
-  :load-path "modules/native/cmake"
-  :after c++-mode)
+(use-package cmake-mode)
+
+(use-package cc-mode
+  :defer t
+  :hooks (hs-minor-mode hideshowvis-minor-mode))
+  
+(use-package irony
+  :after cc-mode
+  
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  
+  (defun setup-c-clang-options ()
+    (setq irony-additional-clang-options (quote ("-std=c11"))))
+
+  (defun setup-cpp-clang-options ()
+    (setq irony-additional-clang-options (quote ("-std=c++14" "-stdlib=libc++"))))
+  
+  :hooks (smartparens-mode
+          company-irony-setup-begin-commands
+          irony-cdb-autosetup-compile-options)
+  
+  :config
+  (add-hook 'c++-mode-hook 'setup-cpp-clang-options)
+  (add-hook 'c-mode-hook 'setup-c-clang-options))
+
+(use-package company-irony
+  :after (irony company)
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
+;; (use-package company-irony-c-headers
+;;   :ensure t
+;;   :after company
+;;   :config (progn
+;;             (setq company-irony-c-headers--compiler-executable (executable-find "clang++"))
+;;             ;; group with company-irony but beforehand so we get first pick
+;;             (add-to-list 'company-backends '(company-irony-c-headers company-irony))))
+
+;; C# Language Support for Unity3D on mac
+(use-package csharp-mode
+  :defer t
+  :mode ("\\.cs$" . csharp-mode)
+  :hooks (hs-minor-mode hideshowvis-enable smartparens-mode))
+
+(use-package omnisharp
+  :after csharp-mode
+  :init
+  ;; Dirty hack for omnisharp compatibility
+  (use-package shut-up  :ensure t))
