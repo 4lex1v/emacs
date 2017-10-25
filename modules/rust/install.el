4344e14f09ab88e92d7b283b927b707c82d5c6d7
Aleksandr Ivanov <4lex1v@gmail.com>
2017-10-25 01:59:20 -0400

    [Rust] More rust configuration =)!

--- modules/rust/install.el
+++ modules/rust/install.el
@@ -1,5 +1,15 @@
 
 (use-package rust-mode :defer t
+  :hooks (;; Rust-specific modes
+          cargo-minor-mode
+          racer-mode
+          
+          ;; General Modes
+          hs-minor-mode
+          hideshowvis-enable
+          yas-minor-mode
+          smartparens-mode
+          company-mode)
   :init 
   (setq rust-indent-offset 2)
   
@@ -7,6 +17,11 @@
   (sp-with-modes 'rust-mode
     (sp-local-pair "(" nil :post-handlers '(("||\n[i]" "RET")))
     (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))))
+
+(use-package smartparens-rust
+  :after (rust-mode smartparens-mode)
+  :config
+  (add-hook 'rust-mode #'smartparens-rust))
 
 (use-package cargo :defer t :ensure t
   :after rust-mode
@@ -29,7 +44,9 @@
    "t" 'cargo-process-test)))
 
 (use-package racer :defer t :ensure t
-  :after rust-mode)
+  :after rust-mode
+  :config
+  (add-hook 'racer-mode-hook #'eldoc-mode))
 
 (use-package flycheck-rust :defer t :ensure t
   :after (rust-mode flycheck-mode)
