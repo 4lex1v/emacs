975f938b0ba98e2e10d855604ed4a6a5ae9f4266
Aleksandr Ivanov <4lex1v@gmail.com>
2017-10-25 01:48:13 -0400

    [Rust] Small improvements to Rust editing experience

--- modules/rust/install.el
+++ modules/rust/install.el
@@ -1,8 +1,30 @@
 
-(use-package rust-mode
-  :defer t
-  :mode ("\\.rs\\'" . rust-mode))
+(use-package rust-mode :defer t)
 
-(use-package racer
-  :ensure t
+(use-package cargo :defer t :ensure t
+  :after rust-mode
+  :general
+  (:prefix "," :keymaps 'rust-mode-map
+   "c." 'cargo-process-repeat
+   "cC" 'cargo-process-clean
+   "cX" 'cargo-process-run-example
+   "cc" 'cargo-process-build
+   "cd" 'cargo-process-doc
+   "ce" 'cargo-process-bench
+   "cf" 'cargo-process-current-test
+   "cf" 'cargo-process-fmt
+   "ci" 'cargo-process-init
+   "cn" 'cargo-process-new
+   "co" 'cargo-process-current-file-tests
+   "cs" 'cargo-process-search
+   "cu" 'cargo-process-update
+   "cx" 'cargo-process-run
+   "t" 'cargo-process-test)))
+
+(use-package racer :defer t :ensure t
   :after rust-mode)
+
+(use-package flycheck-rust :defer t :ensure t
+  :after (rust-mode flycheck-mode)
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
