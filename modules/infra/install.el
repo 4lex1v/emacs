fb54b3b172cd3b036493ab093a2abb72ad509a2a
Aleksandr Ivanov <4lex1v@gmail.com>
2017-11-03 10:36:50 -0400

    [Infra] Enhanced EShell prompt Introduces a git integration

--- modules/infra/install.el
+++ modules/infra/install.el
@@ -26,14 +26,24 @@
     (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete)
     (define-key eshell-mode-map (kbd "M-p") 'helm-eshell-history))
 
+  (defun git-prompt-branch-name ()
+    "Get current git branch name"
+    (let ((args '("symbolic-ref" "HEAD" "--short")))
+      (with-temp-buffer
+        (apply #'process-file "git" nil (list t nil) nil args)
+        (unless (bobp)
+          (goto-char (point-min))
+          (buffer-substring-no-properties (point) (line-end-position))))))
+  
   (defun 4lex1v:eshell-prompt ()
-    (concat
-     "\n# " (user-login-name) " in " (abbreviate-file-name (eshell/pwd)) "\n"
-     ">> " ;; User's input
-     ))
+    (let ((branch-name (git-prompt-branch-name)))
+      (concat
+       "\n# " (user-login-name) " in " (abbreviate-file-name (eshell/pwd)) "\n"
+       (if branch-name (format "git:(%s) >> " branch-name) ">> ")
+       )))
   
   (setq eshell-prompt-function #'4lex1v:eshell-prompt
-        eshell-prompt-regexp "^>>+ ")
+        eshell-prompt-regexp ".*>>+ ")
   
   :hooks (4lex1v:helm-eshell-history
           ansi-color-for-comint-mode-on)
