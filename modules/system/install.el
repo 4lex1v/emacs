58a94e468a59cb2a89af56ff03e6c171cd22d1ff
unknown <4lex1v@gmail.com>
2017-12-09 14:35:39 -0500

    [System] Some windows related configuration

--- modules/system/install.el
+++ modules/system/install.el
@@ -24,6 +24,8 @@
 
 (use-package osx :if IS_MAC
   :init
+  (message "Loading MacOS system configuration")
+
   (setq browse-url-browser-function 'browse-url-default-macosx-browser
         delete-by-moving-to-trash    t
         mac-command-modifier        'meta
@@ -42,7 +44,18 @@
     (eshell/alias "bubu" "brew update && brew upgrade")
     (eshell/alias "sshs" "ssh-add ~/.ssh/github_rsa")))
 
+(use-package windows :if IS_WINDOWS
+  :init
+  (message "Loading Windows OS system configuration")
+  
+  ;; #NOTE(4lex1v) :: Not sure if these paths should be defined here or in Native modules configuration
+  (setq win32-system-include-paths '("c:/Program Files (x86)/Windows Kits/10/Include/10.0.16299.0/shared"
+                                     "c:/Program Files (x86)/Windows Kits/10/Include/10.0.16299.0/ucrt"
+                                     "c:/Program Files (x86)/Windows Kits/10/Include/10.0.16299.0/um"
+                                     "c:/Program Files (x86)/Windows Kits/10/Include/10.0.16299.0/winrt"))
+  :config
+  )
+
 (use-package term
   :config
   (add-hook 'term-load-hook 'term-line-mode))
-
