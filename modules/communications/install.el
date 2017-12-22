ecf1c2765c1acad2b42b729c85b020a66556a603
Aleksandr Ivanov <4lex1v@gmail.com>
2017-12-22 10:37:56 -0500

    Fix modules and remove communications

--- modules/communications/install.el
+++
@@ -1,7 +0,0 @@
-(use-package jabber
-  :defer t
-  :if IS_MAC
-  :after exec-path-from-shell
-  :init (setq jabber-account-list
-              `(("4lex1v@livecoding.tv"
-                 (:password . ,(exec-path-from-shell-getenv "LC_PWD"))))))
