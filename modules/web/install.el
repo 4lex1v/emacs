6ac378e6aea261c9ac548890208e65b16b597aa8
Aleksandr Ivanov <4lex1v@gmail.com>
2018-02-12 09:42:05 -0500

    [Web] Enable Restclient in Org

--- modules/web/install.el
+++ modules/web/install.el
@@ -5,29 +5,26 @@
    ("\\.mustache\\'" . web-mode)
    ("\\.jinja\\'" . web-mode)
    ("\\.php\\'" . web-mode))
+  
   :init
   (setq web-mode-engines-alist '(("\\.jinja\\'"  . "django"))
         web-mode-enable-auto-pairing t
         web-mode-enable-css-colorization t
         web-mode-enable-current-element-highlight t
-        web-mode-enable-current-column-highlight nil)
-
-  (with-mode which-key
-    (which-key-declare-prefixes-for-mode 'web-mode
-      "C-c C-t" "web/tag"
-      "C-c C-b" "web/block"
-      "C-c C-d" "web/dom"
-      "C-c C-a" "web/attribute"
-      "C-c C-e" "web/element")))
+        web-mode-enable-current-column-highlight nil))
 
 ;; Can't get it from the repos...
 (use-package tide :ensure t :disabled t
   :init
-  (with-eval-after-load evil-collection
+  (with-eval-after-load 'evil-collection
     (add-to-list 'evil-collection-mode-list 'tide)
     (add-to-list 'evil-collection-mode-list 'typescript-mode)))
 
 (use-package restclient :defer t
-  :init
-  (use-package ob-restclient :after ob))
+  :commands restclient-mode
+  :config
+  (with-eval-after-load 'org
+    (add-to-list 'org-babel-load-languages '(restclient . t))))
+
+(use-package ob-restclient :after (:both org restclient))
     
