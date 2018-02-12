c8fd4f165fe66fd0d789876ca6b8ed23b00f7885
Aleksandr Ivanov <4lex1v@gmail.com>
2018-02-12 17:19:56 -0500

    [Reader] Proper config

---
+++ modules/reader/install.el
@@ -0,0 +1,13 @@
+
+;; #NOTE :: This package requires a bunch of side things to be cooked, using the melpa's version
+;; provides and install script that configures all that automatically.
+(use-package pdf-tools :ensure t
+  :mode (("\\.pdf\\'" . pdf-view-mode))
+  :config
+  (pdf-tools-install)
+  
+  (with-eval-after-load 'evil-collection
+    (require 'evil-collection-pdf)
+    (evil-collection-pdf-setup)))
+
+
