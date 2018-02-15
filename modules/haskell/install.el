161913b4c9568ca4c8eaa4f1ebd8dfda41f702e2
Aleksandr Ivanov <4lex1v@gmail.com>
2018-02-15 16:32:39 -0500

    [Haskell] Cleanup config

--- modules/haskell/install.el
+++ modules/haskell/install.el
@@ -1,35 +1,19 @@
-(use-package haskell-mode-autoloads
-  :defer)
-
 (use-package haskell-mode
-  :defer 
   :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
          ("\\.lhs\\'" . literate-haskell-mode)
          ("\\.cabal\\'" . haskell-cabal-mode))
 
+  :hooks
+  (interactive-haskell-mode
+   haskell-doc-mode
+   haskell-decl-scan-mode)
+  
   :init
   (setq haskell-process-log t
         haskell-process-type 'stack-ghci)
   
-  (eval-after-load 'org-mode
-    (lambda ()
-      (add-to-list 'org-babel-load-languages '(haskell . t))))
+  :config
+  (evil-set-initial-state 'haskell-error-mode 'emacs)
   
-  :config
-  (evil-set-initial-state 'haskell-error-mode 'emacs))
-
-(use-package haskell
-  :defer
-  :config
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
-
-(use-package haskell-doc-mode
-  :defer
-  :config
-  (add-hook 'haskell-mode-hook 'haskell-doc-mode))
-
-(use-package haskell-decl-scan
-  :defer
-  :config
-  (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode))
-
+  (with-eval-after-load 'org
+    (add-to-list 'org-babel-load-languages '(haskell . t))))
