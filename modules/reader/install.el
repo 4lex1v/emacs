c72429d660b6b3b74948e91c9bb1350ee7956803
Aleksandr Ivanov <4lex1v@gmail.com>
2018-05-23 10:34:39 -0400

    [Reader] Drop PDF reader support + better elfeed

--- modules/reader/install.el
+++ modules/reader/install.el
@@ -1,12 +1,29 @@
 
-;; #NOTE :: This package requires a bunch of side things to be cooked, using the melpa's version
-;; provides and install script that configures all that automatically.
-(use-package pdf-tools :ensure t
-  :mode (("\\.pdf\\'" . pdf-view-mode))
+(use-package elfeed
+  :general
+  ("se" 'elfeed)
+
+  (:keymaps 'elfeed-search-mode-map
+   :states  'normal
+   :prefix   nil
+
+   "SPC" 'elfeed-control-panel/body)
+  
+  :init 
+  (setq elfeed-search-filter "@1-month-ago +unread" ;; Default filter
+        elfeed-feeds '(("https://bartoszmilewski.com/feed" FP)
+
+        elfeed-db-directory "~/Dropbox/Приложения/elfeeddb"
+        
+        elfeed-mac-connections 10
+        url-queue-timeout 30)
+
+  (defhydra elfeed-control-panel ()
+    "elfeed"
+    ("u" elfeed-update "update" :color blue)
+    ("q" nil "quit" :color blue))
+  
   :config
   (with-eval-after-load 'evil-collection
-    (require 'evil-collection-pdf)
-    (evil-collection-pdf-setup))
-  
-  (pdf-tools-install))
-
+    (add-to-list 'evil-collection-mode-list 'elfeed)
+    (evil-collection-init)))
