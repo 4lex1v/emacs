1740afd0f7f2573d572a9faded9a95e11cfa3750
Aleksandr Ivanov <4lex1v@gmail.com>
2019-01-09 16:56:27 -0500

    Remove custom vars

--- init.el
+++ init.el
@@ -2182,23 +2182,3 @@ _v_: Visualize
                               '(company-files company-dabbrev)))))))
 
 (setq gc-cons-threshold 1000000)
-
-
-
-
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-source-names-using-follow (quote ("Occur"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
